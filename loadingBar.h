844a1a1e40592232bd4cd70e35166e21ecfc2f20
Rayon-Richter Malou <rayon.richter.malou@gmail.com>
2019-05-21 22:56:18 +0200

    add comments into loadingBar.h

--- loadingBar.h
+++ loadingBar.h
@@ -3,11 +3,25 @@
 class loadingBar
 {
 public:
+	/* constructor for the loadingBar object
+	 * text is the text displayed during loading
+	 * iterations is the number of steps for the loadingBar to be full
+	 */
 	loadingBar(std::string text="Loading...", int iterations=100);
+	/* return a string with the loadingBar state
+	 */
 	std::string getState();
+	/* increase the loadingBar state of points steps and return a string with the loadingBar state
+	 */
 	std::string increase(int points=1);
+	/* decrease the loadingBar state of points steps and return a string with the loadingBar state
+	 */
 	std::string decrease(int points=1);
+	/* return the number of steps for the loadingBar to be full
+	 */
 	int getMaxAdvancement() const;
+	/* return the current number of steps of the loadingBar
+	 */
 	int getAdvancement() const;
 private:
 	std::string m_text;
